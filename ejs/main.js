d04d87f8ac0f3c6186f369ebf5e86f29bf5110f3
Rengel Maricela <maricela.rengel@gmail.com>
2022-11-13 23:21:49 -0300

    Handlebars - Pug - Ejs

--- ejs/main.js
+++ ejs/main.js
@@ -1,20 +1,14 @@
 const express = require ('express');
-const handlebars = require('express-handlebars');
-
 const app = express();
 
 const PUERTO = process.env.PORT || 8080;
 
 
-
-//***** Hacemos la carpeta public visible
-app.use('/static', express.static(__dirname + '/public'));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+// app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs'); 
 
 
 class Contenedor {
@@ -72,7 +66,6 @@ class Contenedor {
         if (indexSeleccionado != -1) {
         this.productos.splice(indexSeleccionado, 1);
         }
-
     }
 
     deleteAll() {
@@ -82,27 +75,25 @@ class Contenedor {
 
 const productos = new Contenedor([]);
 
-//Datos de prueba
-productos.save({
-    title: 'PC',
-    price: '4000',
-    thumbnail: 'image1'
+app.get('/productos', (peticion, respuesta) => {
+    const listaProductos = productos.getAll();
+    respuesta.render('lista', {
+        productos: listaProductos
+    });
 });
 
-productos.save({
-    title: 'Teclado',
-    price: '400',
-    thumbnail: 'image2'
+app.post('/', (peticion, respuesta) => {
+    const producto = peticion.body;
+    productos.save(producto);
+    respuesta.render('formulario', {});
 });
 
-
-
-
-
+app.get('/', (peticion, respuesta) => {
+    respuesta.render('formulario', {});
+});
 
 const servidor = app.listen(PUERTO, () => {
     console.log(`Servidor escuchando: ${servidor.address().port}`);
 });
 
 servidor.on('error', error => console.log(`Error: ${error}`));
-
